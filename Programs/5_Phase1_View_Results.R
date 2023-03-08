1b693079143b01349e86502d3b3e9acd4a015e01
ynchen08 <ynchen08@gmail.com>
2023-03-08 11:31:53 -0500

    Update program folder

--- Programs/5_Phase1_View_Results.R
+++ Programs/5_Phase1_View_Results.R
@@ -22,6 +22,10 @@ library(here)
 library(lcmm)
 library(ggplot2)
 library(ggpubr)
+# install.packages("tidyLPA")
+library(tidyLPA)
+# install.packages('mclust')
+library(mclust)
 
 #import plot theme set-up
 source(here("misc/plot_theme.R"))
@@ -155,26 +159,29 @@ npar=fit[[2]]$npm
 Da=data.frame(K=1:6,ABIC=  (-2*llik) + ((log((4000 + 2)/24)) * npar))
 ggplot(data=Da,aes(x=K,ABIC))+geom_line()+geom_point()+xlab("K") + ylab("aBIC")+scale_x_continuous(breaks=1:6)+theme_ben()+ggtitle("Scree plot of sample size-adjusted BIC")+theme(plot.title = element_text(hjust = 0.5,margin=margin(0,0,25,0)))
 
+# Lo–Mendell–Rubin likelihood ratio test - originally designed in a study testing the number of components in a normal mixture, and has been widely used in latent profile analysis. Not sure if the model type (probit) would affect its performance?  
+LRT_run=length(llik)-1
 
-install.packages("tidyLPA")
-library(tidyLPA)
-
-install.packages('mclust')
-library(mclust)
-
-llik
-
-calc_lrt(n=4000,
-         null_ll=llik[1],
-         null_param = npar[1],
-         null_classes = 1,
-         alt_ll=llik[2],
-         alt_param = npar[2],
-         alt_classes = 2)
-
-?calc_lrt
+LRT=data.frame(Test=c("1v2","2v3","3v4","4v5","5v6"),
+               aLMR=rep(0,LRT_run),
+               df=rep(0,LRT_run),
+               p=rep(0,LRT_run))
 
 
+for (i in 1:LRT_run){
+  lrt=calc_lrt(n=4000,
+                  null_ll=llik[i],
+                  null_param = npar[i],
+                  null_classes = i,
+                  alt_ll=llik[i+1],
+                  alt_param = npar[i+1],
+                  alt_classes = (i+1))
+  LRT$aLMR[i]=lrt[2]
+  LRT$df[i]=lrt[3]
+  LRT$p[i]=lrt[4]
+}
+
+LRT
 #################################################################################################
 #Extract model coefficients
 
--- Programs/6_Association_Test.R
+++ Programs/6_Association_Test.R
@@ -45,7 +45,7 @@ new_ID=SeroProtect$ID
 sampled_ID_index=data.frame(ID_orig=orig_ID,
                     ID=new_ID)
 
-write.csv(sampled_ID_index, here("Export","Sampled_ID_index.csv"), row.names=FALSE)
+# write.csv(sampled_ID_index, here("Export","Sampled_ID_index.csv"), row.names=FALSE)
 
 #Convert wide to long format (with analysis ID) --------------------------------------------
 SP_long=SeroProtect%>%tidyr::gather(., Week, Protect,Protect1:Protect103, factor_key=TRUE)
@@ -54,7 +54,7 @@ SP_long=SP_long%>%arrange(ID,Week)
 ##scale down the time variable to facilitate model convergence
 SP_long$Week=SP_long$Week/10  
 
-saveRDS(SP_long,here("./Data/SP_long_4k"))
+# saveRDS(SP_long,here("./Data/SP_long_4k"))
 
 
 # Append covariates to the long dataset -----------------------------------------------------
@@ -64,8 +64,7 @@ SP_long3=merge(SP_long2,y=covar,by="ID_orig",all.x=TRUE)%>%dplyr::select(ID,Week
 
 
 #Run 1-group latent class trajectory model ---------------------------------------------------
-mod=list()
-mod[[1]]=lcmm(Protect~1+Week+I(Week^2), subject='ID',ng=1,data=SP_long, link="thresholds", maxiter=200)  
+init_mod=lcmm(Protect~1+Week+I(Week^2), subject='ID',ng=1,data=SP_long3, link="thresholds", maxiter=200)  
 
 
 # Implement K=2~6 GBTM--------------------------------------------------------------------------
@@ -74,26 +73,59 @@ source(here("Programs","Helper_Functions_GBTM.R"))
 num_rep=20
 num_maxit=10
 Ncore=detectCores()-1
+k=4
 
-Time_rep20maxit10=rep(NA,6)
-mod[[2]]=mod[[3]]=mod[[4]]=mod[[5]]=mod[[6]]=NULL
+t1=Sys.time()
+cl=makeCluster(Ncore-1)
+clusterExport(cl,list("k",'lcmm'),environment())
+mod=gridsearch(rep = num_rep, maxiter = num_maxit, minit = init_mod,
+               lcmm(fixed=Protect~1+Week+I(Week^2),random=~-1, mixture=~1+Week+I(Week^2),
+                    subject='ID',
+                    # classmb =~Age_at_init_cat+Average_copay_cat+Primary_payer+Pharmacy_type,
+                    ng=k,data=SP_long3, link="thresholds",nwg=FALSE),cl=cl)
 
-for (k in 2:6){
-  cat("K=",k)
-  t1=Sys.time()
-  cl=makeCluster(Ncore-1)
-  clusterExport(cl,list("k",'lcmm'),environment())
-  mod[[k]]=gridsearch(rep = num_rep, maxiter = num_maxit, minit = mod[[1]],
-                      lcmm(fixed=Protect~1+Week+I(Week^2),random=~-1, mixture=~1+Week+I(Week^2),
-                           subject='ID',ng=k,data=SP_long, link="thresholds",nwg=FALSE),cl=cl)
-  stopCluster(cl)
-  t2=Sys.time()
-  Time_rep20maxit10[k]=difftime(t2,t1,units ='mins')
-}
+mod_assoc=gridsearch(rep = num_rep, maxiter = num_maxit, minit = init_mod,
+               lcmm(fixed=Protect~1+Week+I(Week^2),random=~-1, mixture=~1+Week+I(Week^2),
+                    subject='ID',
+                    classmb =~Age_at_init_cat+Average_copay_cat+Primary_payer+Pharmacy_type,
+                    ng=k,data=SP_long3, link="thresholds",nwg=FALSE),cl=cl)
+stopCluster(cl)
+t2=Sys.time()
 
-saveRDS(mod,here("./Export/GBTM_data4k_rep20maxit10"))
+RunTime=difftime(t2,t1,units ='mins')
+
+
+saveRDS(mod_assoc,here("./Export/GBTM_fake_assoc"))
 # mod=readRDS(here("./Export/GBTM_data4k_rep20maxit10"))
 
-Modfit_data4k_rep20maxit10=GBTM_stat(mod)
-ExportStuff=list(Time_rep20maxit10,Modfit_data4k_rep20maxit10)
-saveRDS(ExportStuff,here("./Export/Stats_data4k_rep20maxit10"))
+
+Modfit_assoc=GBTM_stat(mod_assoc)
+
+summary(mod[[3]])
+summary(mod_assoc[[3]])
+parameters(mod[[2]])
+
+mod[[2]]$conv
+
+mod_assoc[[2]]$conv
+
+
+realmod=readRDS(here("Export","real","GBTM_data4k_rep20maxit10"))
+
+mod[[3]]$best
+
+num_param=mod[[2]]$best%>%length()
+UT=mod[[2]]$V
+
+vcov=matrix(0,num_param,num_param)
+
+vcov[upper.tri(vcov,diag=TRUE)]=UT
+
+f_vcov=vcov+t(vcov)
+diag(f_vcov)=diag(f_vcov)-diag(vcov)
+
+
+
+
+
+
